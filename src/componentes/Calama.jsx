d5634e11a5ac2a57e1e4ac475eab344cedee7946
LLULYAN ANDRES CARRILLO CROSS BUCHANAN <llulyan.carrillo@inacapmail.cl>
2023-07-10 18:20:31 -0400

    Añadidad rutas y componentes

---
+++ src/componentes/Calama.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+//function HolaMundo {}
+
+/*Vista*/
+const Calama = () => {
+    return (
+        <>
+        <h1>Jalama</h1>
+        {/*<p className='algo'></p> ahora la etiqueta class tiene no funciona si la etiqueta class no lleva name*/}
+        </>
+    )
+}
+export default ciudad
---
+++ src/componentes/HolaMundo.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+
+//function HolaMundo {}
+
+/*Vista*/
+const HolaMundo = () => {
+    return (
+        <>
+        <h1>Hola Mundo</h1>
+        {/*<p className='algo'></p> ahora la etiqueta class tiene no funciona si la etiqueta class no lleva name*/}
+        </>
+    )
+}
+export default HolaMundo
